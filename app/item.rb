7ffb77a19ae5a12296a2a3d712c1f4592c1d25c7
meryldakin <meryl.k.dakin@gmail.com>
2017-04-03 12:08:08 -0400

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,14 @@
 class Item
   attr_accessor :name, :price
+  ALL = []
 
   def initialize(name,price)
     @name = name
     @price = price
+    ALL << self
+  end
+
+  def self.all
+    ALL
   end
 end
